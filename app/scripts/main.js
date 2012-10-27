28cd527b88f03623d65182628df656f0c07e7b70
Alexandre Girard <git@alexgirard.com>
2012-10-27 19:48:11 +0200

    add method to send email

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -13,4 +13,12 @@ require(['app'], function(app) {
   // use app here
   console.log(app);
   $(".carousel").carousel();
+  $(".btn.order").click(function() {
+    var comment = $(this).parents(".well"). find("textarea").val();
+    var title = "TetaShirt - " + $(this).parents(".span8").find("h2").text();
+    var link = "mailto:contact@tetalab.org?"
+             + "&subject=" + encodeURIComponent(title)
+             + "&body=" + encodeURIComponent(comment);
+    window.location.href = link;
+  });
 });
